f4cd14ca6ef532617055110b978bf0d206e04742
Solene Duprat <solene.duprat@gmail.com>
2020-05-12 12:35:33 -0400

    Lunch break, still working on it

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,18 @@
 class TasksController < ApplicationController
-
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+  def new
+    @task = Task.new
+  end
+  def create
+    new_task = Task.create(task_params)
+    redirect_to task_path(new_task)
+  end
+  def index
+    @tasks = Task.all
+  end
+  def show
+    @task = Task.find(params[:id])
+  end
 end
